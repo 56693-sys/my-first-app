7d93bfb8b52c035dbc72804f0d1934a8d7500aa3
56693-sys <56693@yupparaj.ac.th>
2026-09-09 11:52:57 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -33,23 +33,23 @@ u_ans2 = ans2.strip().lower()
 if u_ans1 == "apple":
         st.success("✅ ข้อ 1: ถูกต้อง")
         score += 1
-    else:
+else:
         st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")
 
 # ตรวจข้อ 2
 if u_ans2 == "fish":
         st.success("✅ ข้อ 2: ถูกต้อง")
         score += 1
-    else:
+else:
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
 # ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มตรวจข้อ 3, 4 ตรงนี้
 
     st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
 
-    if score == 2:
+if score == 2:
         st.success("🎉 You win!")
-    else:
+else:
         st.error("💀 You lose!")
 
 
@@ -62,9 +62,9 @@ st.button("🎮 เริ่มเล่นเกม", on_click=reset_game)
 if "start" in st.session_state and not st.session_state.get("is_ended", False):
     time_left = int(30 - (time.time() - st.session_state.start))
 
-    if time_left > 0:
+if time_left > 0:
         st.error(f"⏳ เหลือเวลา: {time_left} วินาที")
-    else:
+else:
         st.session_state.is_ended = True
         st.rerun()
 
