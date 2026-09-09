4dd5f57d38275ef6c378db3e7360cf318ba50bef
56693-sys <56693@yupparaj.ac.th>
2026-09-09 11:59:19 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -42,14 +42,14 @@ def show_result_dialog(ans1, ans2):
     u_ans2 = ans2.strip().lower()
 
     # ตรวจข้อ 1
-    if u_ans1 == "apple":
+    if u_ans1 == "lemone":
         st.success("✅ ข้อ 1: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")
 
     # ตรวจข้อ 2
-    if u_ans2 == "fish":
+    if u_ans2 == "pen":
         st.success("✅ ข้อ 2: ถูกต้อง")
         score += 1
     else:
